32891d28858d818bd237ef4f4efd273dd7990a20
frskaaulia <117558877+frieskaauliaputri@users.noreply.github.com>
2023-08-22 11:31:04 +0700

    Delete fl5.php

--- fl5.php
+++
@@ -1,9 +0,0 @@
-<?php
-    for($bil = 1; $bil <= 50; $bil++){
-        if( ($bil % 2) == 0){
-        echo "$bil  Bilangan genap<br>";
-        }else {
-		echo "$bil Bilangan ganjil<br>";
-	    }	
-}
-?>

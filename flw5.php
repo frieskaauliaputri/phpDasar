b2d8fda5e7f32ca394987a62f197d63eec36faaa
frieskaauliaputri <frieskaaulia08@gmail.com>
2023-08-22 11:34:58 +0700

    tambah satu file

---
+++ flw5.php
@@ -0,0 +1,9 @@
+<?php
+    for($bil = 1; $bil <= 50; $bil++){
+        if( ($bil % 2) == 0){
+        echo "$bil  Bilangan genap<br>";
+        }else {
+		echo "$bil Bilangan ganjil<br>";
+	    }	
+}
+?>

aafa74934f00d2246375da9ddb0c62bf1ebb589e
frieskaauliaputri <frieskaaulia08@gmail.com>
2023-08-23 13:34:33 +0700

    perubahan judul

--- flw4.php
+++ flw4.php
@@ -8,7 +8,7 @@ $waktu; $jam; $menit; $detik; $hasil = "";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>mencari jam menit</title>
 </head>
 <body>
     <form action="" method="post">

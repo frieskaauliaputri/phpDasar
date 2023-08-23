f48f3517e16c448d22bfb3f5be1eed6d508dd1f2
frieskaauliaputri <frieskaaulia08@gmail.com>
2023-08-23 13:25:01 +0700

    perubahan

--- flw4.php
+++ flw4.php
@@ -32,12 +32,15 @@ if($waktu >= 3600){
     $waktu -= ($jam * 3600);
     $hasil .= $jam . " jam ";
     // operator += bersifat menambah, kalau hanya = bersifat menimpa
+
+    if($waktu >= 60){
+        $menit = floor($waktu / 60);
+        $waktu -= ($menit * 60);
+        $hasil .= $menit . " menit ";
+    }
 }
-else if($waktu >= 60){
-    $menit = floor($waktu / 60);
-    $waktu -= ($menit * 60);
-    $hasil .= $menit . " menit ";
-}
+
+
     $detik = $waktu;
     $hasil .= $detik . " detik "; 
 
